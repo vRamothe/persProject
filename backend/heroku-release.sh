82035f36f3fa6f4af49fd64e1017a35891986641
vRamothe <ramothe.vivien@gmail.com>
2026-03-28 20:26:49 +0100

    feat: automate static file collection in Dockerfile and remove from heroku-release script

--- backend/heroku-release.sh
+++ backend/heroku-release.sh
@@ -19,7 +19,4 @@ python manage.py migrate --noinput --verbosity 2
 echo ">> Loading seed data"
 python manage.py seed_data
 
-echo ">> Collecting static files"
-python manage.py collectstatic --noinput --clear --verbosity 2
-
 echo ">> Release phase completed"
